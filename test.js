eae6d8635bf82d4e02a357ca5a335dd4d8372386
Juan Batiz-Benet <juan@benet.ai>
2014-06-11 02:15:47 -0700

    added tests :)

---
+++ test.js
@@ -0,0 +1,54 @@
+var test = require('tape')
+var map = require('lodash.map')
+var multiDgrams = require('multi-dgram-stream')
+var msgproto = require('./')
+var Message = msgproto.Message
+var Frame = msgproto.Frame
+
+function setupStreams(addrs) {
+  addrs = map(addrs, function(a) {
+    return 'localhost:' + a
+  })
+
+  return map(addrs, function (addr) {
+    var mds = multiDgrams(addr, addrs)
+    return msgproto.WireProtocol(msgproto.Frame, mds)
+  })
+}
+
+
+test('test send', function(t) {
+  var numMessages = 10
+  t.plan(numMessages * 4 + 1)
+
+  var sent = {}
+  var streams = setupStreams([1234, 2345, 3456])
+  map(streams, function(s) {
+    s.on('data', function(msg) {
+      t.ok(sent[msg.payload], 'should receive msg: ' + msg.payload)
+      sent[msg.payload].push(s)
+
+      if (sent[msg.payload].length == streams.length) { // all got it.
+        delete sent[msg.payload]
+        t.ok(!sent[msg.payload], 'should be done with msg: ' + msg.payload)
+      }
+
+      if (Object.keys(sent).length == 0) { // all done
+        map(streams, function(s) {
+          s.write(null)
+          s.middle.end() // why doesn't s.end() work!?
+        })
+        t.ok(true, 'should be done')
+      }
+    })
+  })
+
+  for (var i = 0; i < numMessages; i++) {
+    var msg = Frame(new Buffer('hello there #' + i))
+    sent[msg.payload] = [] // expect things.
+
+    var sender = streams[(i + 1) % streams.length]
+    sender.write(msg)
+    console.log('sent: ' + msg.payload)
+  }
+})
